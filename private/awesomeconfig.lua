4546ba56664d4eafc67cc8e5a3f0abaf3c17d1f4
Wei Zhu <wei@crunchbase.com>
2017-06-27 00:32:56 -0700

    avail private

---
+++ private/awesomeconfig.lua
@@ -0,0 +1,14 @@
+require('private/hyper')
+
+applist = {
+    {shortcut = 'i',appname = 'iTerm'},
+    {shortcut = 'c',appname = 'Calculator'},
+    {shortcut = 'b',appname = 'Google Chrome'},
+    {shortcut = 'a',appname = 'Activity Monitor'},
+    {shortcut = 'w',appname = 'Wechat'},
+    {shortcut = 'p',appname = 'Pycharm'},
+    {shortcut = 'j',appname = 'Intellij IDEA'},
+    {shortcut = 's',appname = 'Slack'},
+}
+
+-- show_resize_tips = false
